4cc221a1b29abede1f6b0420b061603a5002fbc1
Maxim Filimonov <tpaktopsp@gmail.com>
2021-07-25 19:50:44 +0800

    complete first step

--- test/watwitter_web/live/timeline_live_test.exs
+++ test/watwitter_web/live/timeline_live_test.exs
@@ -1,13 +1,14 @@
 defmodule WatwitterWeb.TimelineLiveTest do
-  use WatwitterWeb.ConnCase, async: true
+  use WatwitterWeb.ConnCase
 
   import Phoenix.LiveViewTest
 
   setup :register_and_log_in_user
 
   test "user can visit home page", %{conn: conn} do
-    {:ok, _view, html} = live(conn, "/")
+    {:ok, view, html} = live(conn, "/")
 
     assert html =~ "Home"
+    assert render(view) =~ "Home"
   end
 end
