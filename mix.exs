e471f85e9fdc3124f6e95bff9c67ef4feeefb159
Maxim Filimonov <tpaktopsp@gmail.com>
2021-08-02 22:07:01 +0800

    add test wqatch interactive

--- mix.exs
+++ mix.exs
@@ -34,22 +34,23 @@ defmodule Watwitter.MixProject do
   defp deps do
     [
       {:bcrypt_elixir, "~> 2.0"},
-      {:phoenix, "~> 1.5.6"},
-      {:phoenix_ecto, "~> 4.1"},
       {:ecto_sql, "~> 3.4"},
       {:ex_machina, "~> 2.4", only: :test},
-      {:postgrex, ">= 0.0.0"},
-      {:phoenix_live_view, "~> 0.14.6"},
       {:floki, ">= 0.27.0", only: :test},
-      {:phoenix_html, "~> 2.11"},
-      {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_dashboard, "~> 0.3 or ~> 0.2.9"},
-      {:phx_gen_auth, "~> 0.5", only: [:dev], runtime: false},
-      {:telemetry_metrics, "~> 0.4"},
-      {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:mix_test_interactive, "~> 1.0", only: :dev, runtime: false},
+      {:phoenix_ecto, "~> 4.1"},
+      {:phoenix_html, "~> 2.11"},
+      {:phoenix_live_dashboard, "~> 0.3 or ~> 0.2.9"},
+      {:phoenix_live_reload, "~> 1.2", only: :dev},
+      {:phoenix_live_view, "~> 0.14.6"},
+      {:phoenix, "~> 1.5.6"},
+      {:phx_gen_auth, "~> 0.5", only: [:dev], runtime: false},
+      {:plug_cowboy, "~> 2.0"},
+      {:postgrex, ">= 0.0.0"},
+      {:telemetry_metrics, "~> 0.4"},
+      {:telemetry_poller, "~> 0.4"}
     ]
   end
 
